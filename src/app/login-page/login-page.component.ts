d52d67c001ea1d7be6eac436f28ce1a83b0f4b22
Gian Marco Lucchino <glucchino@pccube.com>
2020-12-15 11:00:49 +0100

    validazione login

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, FormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -20,31 +22,18 @@ export class LoginPageComponent implements OnInit {
   public passwordRequired:any;
 
   loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
+    username: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required] )
   });
 
 
 
   onLogin() {
-    var required = true;
-    this.usernameRequired="";
-    this.passwordRequired=""
-    if(String (this.loginForm.get('username').value).trim()==""){
-      this.usernameRequired = "Username Richiesto";
-      required=false;
-    }
-    if(String (this.loginForm.get('password').value).trim()==""){
-      this.passwordRequired = "Password Richiesta";
-      required=false;
-    }
-    if(required){
       if (this.loginForm.get('username').value === 'esdebitami' && this.loginForm.get('password').value === 'esdebitami') {
         this.router.navigate(['/dashBoard'])
       } else {
         this.name = "Credenziali errate";
       }
-    }
   }
 
 }
