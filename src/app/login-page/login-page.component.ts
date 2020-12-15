52f4f4f6f010c30137612df92125f09745788495
Gian Marco Lucchino <glucchino@pccube.com>
2020-12-15 09:56:31 +0100

    gestione username e password richiesti

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, FormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -17,6 +16,8 @@ export class LoginPageComponent implements OnInit {
 
 
   public name:any;
+  public usernameRequired:any;
+  public passwordRequired:any;
 
   loginForm = new FormGroup({
     username: new FormControl(''),
@@ -26,10 +27,23 @@ export class LoginPageComponent implements OnInit {
 
 
   onLogin() {
-    if (this.loginForm.get('username').value === 'esdebitami' && this.loginForm.get('password').value === 'esdebitami') {
-      this.router.navigate(['/dashBoard'])
-    } else {
-      this.name = "Credenziali errate";
+    var required = true;
+    this.usernameRequired="";
+    this.passwordRequired=""
+    if(String (this.loginForm.get('username').value).trim()==""){
+      this.usernameRequired = "Username Richiesto";
+      required=false;
+    }
+    if(String (this.loginForm.get('password').value).trim()==""){
+      this.passwordRequired = "Password Richiesta";
+      required=false;
+    }
+    if(required){
+      if (this.loginForm.get('username').value === 'esdebitami' && this.loginForm.get('password').value === 'esdebitami') {
+        this.router.navigate(['/dashBoard'])
+      } else {
+        this.name = "Credenziali errate";
+      }
     }
   }
 
